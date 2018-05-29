5863ed18da7462fb6c2f79cc6adb5d571284cd81
Kyle Pearson <pearson791@live.co.uk>
2018-05-29 16:46:41 +0100

    1. added a new section on form1 to preview the data from internal database
    2. row with Kyle's details added for testing purposes

--- MOT Reminder/MOT Reminder/Form1.cs
+++ MOT Reminder/MOT Reminder/Form1.cs
@@ -35,5 +35,20 @@ namespace MOT_Reminder
                 }
                       
         }
+
+        private void tableBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tableBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.messageDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'messageDataSet.Table' table. You can move, or remove it, as needed.
+            this.tableTableAdapter.Fill(this.messageDataSet.Table);
+
+        }
     }
 }
